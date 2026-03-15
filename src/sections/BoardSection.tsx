6c28d5441c77bdff4a3a936d1d3cbbd83cd7009f
LifeDisparity <pdmytro14@gmail.com>
2026-03-15 00:19:15 -0400

    Update leadership headshots with new image set

--- src/sections/BoardSection.tsx
+++ src/sections/BoardSection.tsx
@@ -1,12 +1,12 @@
 const leadershipMembers = [
-  { name: 'Arthur Faynin', role: 'President', image: '/Arthur_Faynin.jpg', email: 'arthur.faynin@baruchmail.cuny.edu' },
+  { name: 'Arthur Faynin', role: 'President', image: '/Arthur.png', email: 'arthur.faynin@baruchmail.cuny.edu' },
   { name: 'Alfred Leong', role: 'Vice-President', image: '/Alfred_Leong.jpg', email: 'alfred.leong@baruchmail.cuny.edu' },
-  { name: 'Alexi Tilly', role: 'Treasurer', image: '/Alexi_Tilly.jpg', email: 'alexandre.tilly@baruchmail.cuny.edu' },
-  { name: 'Abie Lent', role: 'Secretary', image: '/Abie_Lent.jpg', email: 'abraham.lent@baruchmail.cuny.edu' },
-  { name: 'Daniel Shmir', role: 'Data Officer', image: '/Daniel_Shmir.jpg', email: 'daniel.shmir@baruchmail.cuny.edu' },
-  { name: 'Kateryna Chevplianska', role: 'Data Officer', image: '/Kateryna_Chevplianska.png', email: 'kateryna.chevplianska@baruchmail.cuny.edu' },
-  { name: 'Mei Lin Pan', role: 'Marketing Officer', image: '/Mei_Lin_Pan.webp', email: 'meilin.pan@baruchmail.cuny.edu' },
-  { name: 'Dmytro Popov', role: 'Events Officer', image: '/board_08.jpg', email: 'dmytro.popov@baruchmail.cuny.edu' },
+  { name: 'Alexi Tilly', role: 'Treasurer', image: '/Alexiii.png', email: 'alexandre.tilly@baruchmail.cuny.edu' },
+  { name: 'Abie Lent', role: 'Secretary', image: '/Abraham.png', email: 'abraham.lent@baruchmail.cuny.edu' },
+  { name: 'Daniel Shmir', role: 'Data Officer', image: '/Daniel_2.png', email: 'daniel.shmir@baruchmail.cuny.edu' },
+  { name: 'Kateryna Chevplianska', role: 'Data Officer', image: '/Katya.png', email: 'kateryna.chevplianska@baruchmail.cuny.edu' },
+  { name: 'Mei Lin Pan', role: 'Marketing Officer', image: '/Mei.png', email: 'meilin.pan@baruchmail.cuny.edu' },
+  { name: 'Dmytro Popov', role: 'Events Officer', image: '/Dmytroo.jpg', email: 'dmytro.popov@baruchmail.cuny.edu' },
 ];
 
 export default function LeadershipSection() {
