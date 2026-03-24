32105ac07109ef6cdf75a3912d429eaa0d66efad
LifeDisparity <pdmytro14@gmail.com>
2026-03-23 22:03:41 -0400

    Update board member photos to new image files

--- src/sections/BoardSection.tsx
+++ src/sections/BoardSection.tsx
@@ -1,10 +1,10 @@
 const leadershipMembers = [
-  { name: 'Arthur Faynin', role: 'President', image: '/Arthur.png', email: 'arthur.faynin@baruchmail.cuny.edu' },
+  { name: 'Arthur Faynin', role: 'President', image: '/Arthur_Faynin.jpg', email: 'arthur.faynin@baruchmail.cuny.edu' },
   { name: 'Alfred Leong', role: 'Vice-President', image: '/Alfred_Leong.jpg', email: 'alfred.leong@baruchmail.cuny.edu' },
-  { name: 'Alexi Tilly', role: 'Treasurer', image: '/Alexiii.png', email: 'alexandre.tilly@baruchmail.cuny.edu' },
-  { name: 'Abie Lent', role: 'Secretary', image: '/Abraham.png', email: 'abraham.lent@baruchmail.cuny.edu' },
+  { name: 'Alexi Tilly', role: 'Treasurer', image: '/Alexi_Tilly.jpg', email: 'alexandre.tilly@baruchmail.cuny.edu' },
+  { name: 'Abie Lent', role: 'Secretary', image: '/Abie_Lent.jpg', email: 'abraham.lent@baruchmail.cuny.edu' },
   { name: 'Daniel Shmir', role: 'Data Officer', image: '/Daniel_2.png', email: 'daniel.shmir@baruchmail.cuny.edu' },
-  { name: 'Kateryna Chevplianska', role: 'Data Officer', image: '/Katya.png', email: 'kateryna.chevplianska@baruchmail.cuny.edu' },
+  { name: 'Kateryna Chevplianska', role: 'Data Officer', image: '/Katy.jpg', email: 'kateryna.chevplianska@baruchmail.cuny.edu' },
   { name: 'Mei Lin Pan', role: 'Marketing Officer', image: '/Mei.png', email: 'meilin.pan@baruchmail.cuny.edu' },
   { name: 'Dmytro Popov', role: 'Events Officer', image: '/Dmytroo.jpg', email: 'dmytro.popov@baruchmail.cuny.edu' },
 ];
